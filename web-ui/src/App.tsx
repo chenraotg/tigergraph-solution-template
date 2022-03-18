891d619936555c9180493a739fab244465ad18cf
rao chen <rao.chen@tigergraph.com>
2022-03-18 05:39:54 +0000

    fix the warning info

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import config from './config.json';
 import { useEffect } from 'react';
@@ -17,60 +16,24 @@ interface Res {
   }
 }
 
-interface Vertex {
-  v_id: string;
-  v_type: string;
-  attributes: any;
-}
-
-interface Edge {
-  e_type: string;
-  directed: boolean;
-  from_id: string;
-  from_type: string;
-  to_id: string;
-  to_type: string;
-  attribute: any;
-}
-
 function App() {
-  const apiPrefix = `http://${config.server}:${config.serverPort}/api`;
-  const getVertexCardinalityBody = {
-    function: 'stat_vertex_number',
-    type: '*'
-  }
-  const getEdgeCardinalityBody = {
-    function: 'stat_edge_number',
-    type: '*'
-  }
-
-  async function getCardinality(body: {function: string, type: string}) {
-    let res = await axios.post<Res>(`${apiPrefix}/builtins/${config.graphName}`, body, {headers: {'content-type': 'application/x-www-form-urlencoded'}});
-    return res;
-  }
-
-  async function getVertexCount(v_type: string) {
-    let res = await axios.get<Res>(`${apiPrefix}/graph/${config.graphName}/vertices/${v_type}`);
-    return res;
-  }
-
-  async function selectVertex(params: {graphName: string, vertexType: string, vertexId: string}) {
-    let res = await axios.get<Res>(`${apiPrefix}/graph/${params.graphName}/vertices/${params.vertexType}/${params.vertexId}`);
-    return res;
-  }
-
-  async function selectEdge(params: {graphName: string, sourceVertexType: string, sourceVertexId: string, edgeType: string}) {
-    let res = await axios.get<Res>(`${apiPrefix}/graph/${params.graphName}/edges/${params.sourceVertexType}/${params.sourceVertexId}/${params.edgeType}`);
-    return res;
-  }
-
-  let [vertex, setVertex] = useState<Vertex[]>([]);
-  let [edge, setEdge] = useState<Edge[]>([]);
-
   let [vertexCount, setVertexCount] = useState(0);
   let [edgeCount, setEdgeCount] = useState(0);
 
   useEffect(() => {
+    let getVertexCardinalityBody = {
+      function: 'stat_vertex_number',
+      type: '*'
+    }
+    let getEdgeCardinalityBody = {
+      function: 'stat_edge_number',
+      type: '*'
+    }
+    const apiPrefix = `http://${config.server}:${config.serverPort}/api`;
+    async function getCardinality(body: {function: string, type: string}) {
+      let res = await axios.post<Res>(`${apiPrefix}/builtins/${config.graphName}`, body, {headers: {'content-type': 'application/x-www-form-urlencoded'}});
+      return res;
+    }
     getCardinality(getVertexCardinalityBody).then(res => {
       const count = res.data.results[0]?.count;
       setVertexCount(count);
@@ -79,15 +42,6 @@ function App() {
       const count = res.data.results[0]?.count;
       setEdgeCount(count);
     })
-
-    selectVertex({graphName: 'social', vertexType: 'person', vertexId: 'Tom'}).then(res => {
-      setVertex(res.data.results);
-      console.log(res.data.results);
-    });
-    selectEdge({graphName: 'social', sourceVertexType: 'person', sourceVertexId: 'Tom', edgeType: 'friendship'}).then(res => {
-      setEdge(res.data.results);
-      console.log(res.data.results);
-    });
   }, []);
 
 
